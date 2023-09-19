27030959f8f0f0e511f0ecba883f7a536aebd8e9
NguyenSon2310 <thaison5qt@gmail.com>
2023-09-19 15:21:15 +0700

    user referral

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { UserReferral } from './user-referal.entity';
+import { UsersController } from './users.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, UserReferral])],
   providers: [UsersService],
   exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
